7f1915995ab1d3078648736f357704b3fc0f140e
GitbyShantanu <Shantanudofficial5@gmail.com>
2026-02-19 04:17:46 +0530

    shifted Base.metadata.create_all(engine) from db to main.

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from backend.routes.products import router as product_router
+from backend.database import Base, engine
 
 app = FastAPI()
+
+Base.metadata.create_all(engine) # Takes metadata from Base and create all tables.
 
 app.add_middleware(
     CORSMiddleware,
